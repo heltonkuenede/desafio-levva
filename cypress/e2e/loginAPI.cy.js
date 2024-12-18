e3dda268fe38a10ec1d4d7b727e27a4ed84e89a1
HELTON_KUENEDE\helto <heltonkuenede@gmail.com>
2024-12-18 18:57:49 -0300

    teste de API, cenario 2, validação de status code 400, campo senha em branco e validação de mensagem do brack-end

--- cypress/e2e/loginAPI.cy.js
+++ cypress/e2e/loginAPI.cy.js
@@ -1,7 +1,7 @@
 describe('Teste da API - Página de Login', () => {
     const baseUrl = 'https://serverest.dev';
 
-    it('Deve retornar validar status code 400 e mensagem de validação de email do back-end', () => {
+    it('Deve validar status code 400, campo email em branco e mensagem de validação de email do back-end', () => {
         cy.request({
             method: 'POST',
             url: `${baseUrl}/login`,
@@ -16,6 +16,24 @@ describe('Teste da API - Página de Login', () => {
 
             // Inspeciona a estrutura do body para validar o campo correto
             expect(response.body).to.have.property('email', 'email não pode ficar em branco');
+        })
+    })
+
+    it('Deve validar status code 400, campo senha em branco e mensagem de validação de senha do back-end', () => {
+        cy.request({
+            method: 'POST',
+            url: `${baseUrl}/login`,
+            failOnStatusCode: false, // Permite continuar mesmo que o status não seja 2xx
+            body: {
+                email: 'teste@mail.com',
+                password: '', // Senha em branco
+            },
+        }).then((response) => {
+            // Valida o status HTTP
+            expect(response.status).to.eq(400);
+
+            // Inspeciona a estrutura do body para validar o campo correto
+            expect(response.body).to.have.property('password', 'password não pode ficar em branco');
         })
     })
 });
