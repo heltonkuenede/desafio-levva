fa0b930593e049e6c5e99fcfa4bdc3980d6cba27
HELTON_KUENEDE\helto <heltonkuenede@gmail.com>
2024-12-18 18:22:13 -0300

    terceiro e quarto cenario de teste - Campo obrigatório não preenchido - E-mail e Campo obrigatório não preenchido - Nome

--- cypress/e2e/loginFront.cy.js
+++ cypress/e2e/loginFront.cy.js
@@ -35,4 +35,42 @@ describe('Desafio de Automação com Cypress', () => {
             expect('Password é obrigatório').to.equal(msgErro);
         })
     })
+
+    it('Cenário de Teste 03: Cadastrar Usuário - Campo obrigatório não preenchido - Nome', () => {
+
+        // Deve clicar no link 'Cadastrar-se'
+        cy.get('[data-testid="cadastrar"]').click()
+
+        // Deve realizar a validação do campo nome
+        cy.get('[data-testid="nome"]')
+        cy.get('[data-testid="email"]').type('teste@mail.com'); // preenche o campo e-mail
+        cy.get('[data-testid="password"]').type('Teste!@#45'); // preence o campo senha
+        cy.get('[data-testid="cadastrar"]').click() // clica no botão Cadastrar
+
+        cy.wait(1000);
+        // Deve validar se apresenta a mensagem de campo obrigatório
+        cy.get('.alert > :nth-child(2)').invoke('text').then((erro) => {
+            const msgErro = erro.trim();
+            expect('Nome é obrigatório').to.equal(msgErro);
+        })
+    })
+
+    it('Cenário de Teste 04: Cadastrar Usuário - Campo obrigatório não preenchido - E-mail', () => {
+
+        // Deve clicar no link 'Cadastrar-se'
+        cy.get('[data-testid="cadastrar"]').click()
+
+        // Deve realizar a validação do campo nome
+        cy.get('[data-testid="nome"]').type('Teste'); // preenche o campo nome
+        cy.get('[data-testid="email"]')
+        cy.get('[data-testid="password"]').type('Teste!@#45'); // preence o campo senha
+        cy.get('[data-testid="cadastrar"]').click() // clica no botão Cadastrar
+
+        cy.wait(1000);
+        // Deve validar se apresenta a mensagem de campo obrigatório
+        cy.get('.alert > :nth-child(2)').invoke('text').then((erro) => {
+            const msgErro = erro.trim();
+            expect('Email é obrigatório').to.equal(msgErro);
+        })
+    })
 });
